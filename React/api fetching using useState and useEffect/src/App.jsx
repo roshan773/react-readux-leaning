c930636225c8a027e01dc753483526972c8b43c7
Pakhre Roshan <pakhreroshan@gmail.com>
2025-04-19 17:24:27 +0530

    Changes of file name

--- React/api fetching using useState and useEffect/src/App.jsx
+++ React/api fetching using useState and useEffect/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Jokes from './components/Jokes'
+import Product from './components/Product'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Jokes />
+      <Product />
     </>
   )
 }
--- React/api fetching using useState and useEffect/src/components/Product.jsx
+++ React/api fetching using useState and useEffect/src/components/Product.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
-const Jokes = () => {
+const Product = () => {
 
     const [data, setdata] = useState([])
     const [loading, setloading] = useState(false)
@@ -31,4 +31,4 @@ const Jokes = () => {
     )
 }
 
-export default Jokes
+export default Product
