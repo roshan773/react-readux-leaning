2e48ff7e48591d42a643a335f9364ec4d957632e
Pakhre Roshan <pakhreroshan@gmail.com>
2025-04-20 22:54:50 +0530

    added reducer

--- Redux/Demo/index.js
+++ Redux/Demo/index.js
@@ -1,6 +1,28 @@
 const BUY_CAKE = "Buy Cake"
 
+function Buycake(){
+    return {
+        type: BUY_CAKE,
+        info: "First Redux action"
+    }
+}
 
-{
-    type: BUY_CAKE
+console.log(Buycake)
+
+// reducer
+// (previousState, action) => newState
+
+
+const initialstate = {
+    NumofCake: 10,
+}
+
+const reducer = (state=initialstate, action) => {
+    switch(action.type){
+        case BUY_CAKE: return{
+            ...state,
+            NumofCake: state.NumofCake - 1
+        }
+        default: return state
+    }
 }
