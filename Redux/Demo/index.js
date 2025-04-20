688657febeaf655d1db79f8eced511a26b7f40d5
Pakhre Roshan <pakhreroshan@gmail.com>
2025-04-20 23:37:20 +0530

    added the core of redux

--- Redux/Demo/index.js
+++ Redux/Demo/index.js
@@ -1,28 +1,34 @@
-const BUY_CAKE = "Buy Cake"
+const redux = require("redux")
+const {createStore} = redux;
 
-function Buycake(){
-    return {
+const BUY_CAKE = "buy cake"
+
+function buyCake(){
+    return{
         type: BUY_CAKE,
-        info: "First Redux action"
+        info: "First redux action"
     }
 }
 
-console.log(Buycake)
-
 // reducer
-// (previousState, action) => newState
-
 
 const initialstate = {
-    NumofCake: 10,
+    Numofcake : 10,
 }
 
-const reducer = (state=initialstate, action) => {
+const reducer = (state = initialstate, action) => {
     switch(action.type){
         case BUY_CAKE: return{
-            ...state,
-            NumofCake: state.NumofCake - 1
+            Numofcake: state.Numofcake - 1,
         }
         default: return state
     }
 }
+
+const store = createStore(reducer)
+console.log("updated State", store.getState())
+const unsubscribe = store.subscribe(() => console.log("updated state", store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+unsubscribe()
